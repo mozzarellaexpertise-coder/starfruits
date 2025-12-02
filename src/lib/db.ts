fffbff79f7356bb0600f779d1624f0b8485e4a99
GAT <mozzarellaexpertise@gmail.com>
2025-12-02 22:05:32 +0630

    Use dynamic env for Neon DB on Vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,7 @@
-// src/lib/db.ts
 import { neon } from '@neondatabase/serverless';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL not set!');
-const sql = neon(env.DATABASE_URL);
-
-export default sql;
+export function getSql() {
+    if (!env.DATABASE_URL) throw new Error('DATABASE_URL not set!');
+    return neon(env.DATABASE_URL);
+}
