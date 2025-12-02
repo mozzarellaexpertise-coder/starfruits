3ac29036bfc00a2f89509d6e333f5888cf834a3c
GAT <mozzarellaexpertise@gmail.com>
2025-12-02 21:54:45 +0630

    Fix: Vercel env for Neon DB

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,8 @@
+// src/lib/db.ts
 import { neon } from "@neondatabase/serverless";
-import { DATABASE_URL } from '$env/static/private'; 
+import { DATABASE_URL } from '$env/static/private';
 
-const sql = neon(DATABASE_URL); 
+if (!DATABASE_URL) throw new Error('DATABASE_URL not set!');
+const sql = neon(DATABASE_URL);
 
 export default sql;
